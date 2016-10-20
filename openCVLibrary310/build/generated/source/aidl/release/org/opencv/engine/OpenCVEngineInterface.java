7ec3665c976c523bf11461630275b885afae7673
John <johncaingles@gmail.com>
2016-10-20 22:01:26 +0800

    changed gradle, ndk should work for all?? LASARE TIME WHOOOH

--- openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\KingstonAnthony\\Documents\\AndroidStudioProjects\\OpenCVDemo\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\John\\Documents\\Android Projects\\THESIS\\LegoDetection\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
