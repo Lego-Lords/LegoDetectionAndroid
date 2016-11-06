c6e095ffc812f31a1a4d22ce291c82b5d2208b0e
kingstonkoa <kingston_koa@yahoo.com>
2016-11-07 00:52:20 +0800

    contours

--- app/src/main/java/lego/opencvdemo/MainActivity.java
+++ app/src/main/java/lego/opencvdemo/MainActivity.java
@@ -1,57 +1,31 @@
 package lego.opencvdemo;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Paint;
-import android.os.Build;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.SurfaceView;
-import android.view.WindowManager;
-
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.android.*;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
-// OpenCV Classes
 
-public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+import java.util.ArrayList;
+import java.util.List;
 
-    // Used for logging success or failure messages
-    private static final String TAG = "OCVSample::Activity";
-    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
+    private static final String TAG = MainActivity.class.getCanonicalName();
+
     private CameraBridgeViewBase mOpenCvCameraView;
 
-    // Used in Camera selection from menu (when implemented)
-    private boolean              mIsJavaCamera = true;
-    private MenuItem             mItemSwitchCamera = null;
-
-    //FOR IMAGE PROCESSING
-    private int                    mViewMode;
+    private Mat                    mRgba;
     private Mat                    mIntermediateMat;
     private Mat                    mGray;
+    Mat hierarchy;
 
-    // These variables are used (at the moment) to fix camera orientation from 270degree to 0degree
-    Mat mRgba;
-    Mat mRgbaF;
-    Mat mRgbaT;
+
+    List<MatOfPoint> contours;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -70,143 +44,93 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         }
     };
 
-    public MainActivity() {
-        Log.i(TAG, "Instantiated new " + this.getClass());
-    }
-
-
-
-
-    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.show_camera);
-
-        if (checkSelfPermission(Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant
-
-            return;
-        }
-
-        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.show_camera_activity_java_surface_view);
-
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.MainActivityCameraView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! do the
-                    // calendar task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'switch' lines to check for other
-            // permissions this app might request
-        }
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this,
+                mLoaderCallback);
     }
 
-    public void onDestroy() {
-        super.onDestroy();
+    @Override
+    public void onPause() {
+        super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
+
+
+    @Override
     public void onCameraViewStarted(int width, int height) {
-
         mRgba = new Mat(height, width, CvType.CV_8UC4);
-        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
-        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
-
         mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
         mGray = new Mat(height, width, CvType.CV_8UC1);
+        hierarchy = new Mat();
     }
 
+    @Override
     public void onCameraViewStopped() {
         mRgba.release();
         mGray.release();
         mIntermediateMat.release();
+        hierarchy.release();
     }
 
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.gray();
+        contours = new ArrayList<MatOfPoint>();
+        hierarchy = new Mat();
 
-        // TODO Auto-generated method stub
-        mRgba = inputFrame.rgba();
+        Imgproc.Canny(mRgba, mIntermediateMat, 80, 100);
+        Imgproc.findContours(mIntermediateMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+    /* Mat drawing = Mat.zeros( mIntermediateMat.size(), CvType.CV_8UC3 );
+     for( int i = 0; i< contours.size(); i++ )
+     {
+    Scalar color =new Scalar(Math.random()*255, Math.random()*255, Math.random()*255);
+     Imgproc.drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, new Point() );
+     }*/
+        hierarchy.release();
+        Imgproc.drawContours(mRgba, contours, -1, new Scalar(Math.random() * 255, Math.random() * 255, Math.random() * 255));//, 2, 8, hierarchy, 0, new Point());
+        // Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
 
-        //canny dat shit
-//        mRgba = inputFrame.rgba();
-//        Imgproc.Canny(inputFrame.gray(), mIntermediateMat, 80, 100);
-//        Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+        //Imgproc.erode(mRgba, mRgba, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
 
-        //FILTER
-        Mat greyImg = new Mat();
-        Imgproc.cvtColor(mRgba, greyImg, Imgproc.COLOR_BGR2GRAY);
-        double sigmaX = 3;
-        Imgproc.GaussianBlur(greyImg, greyImg, new Size(5, 5), sigmaX);
-//		blur(greyImg, greyImg, new Size(5, 5));
-        Imgproc.adaptiveThreshold(greyImg, greyImg, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 13, 10);
+        Imgproc.dilate(mRgba, mRgba, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
 
-        //EDGES
-        Mat edges = new Mat();
-        Imgproc.Canny(greyImg, edges, 300, 600, 5, true);
+        /*for ( int contourIdx=0; contourIdx < contours.size(); contourIdx++ )
+        {
+            // Minimum size allowed for consideration
+            MatOfPoint2f approxCurve = new MatOfPoint2f();
+            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(contourIdx).toArray() );
+            //Processing on mMOP2f1 which is in type MatOfPoint2f
+            double approxDistance = Imgproc.arcLength(contour2f, true)*0.3;
+            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
 
-        //DRAW LINES
-        Mat lines = new Mat();
-        int threshold = 50;
-        int minLineLength = 75;
-        int maxLineGap = 20;
-        //threshold: The minimum number of intersections to “detect” a line
-        //minLinLength: The minimum number of points that can form a line. Lines with less than this number of points are disregarded.
-        //maxLineGap: The maximum gap between two points to be considered in the same line.
-        Imgproc.HoughLinesP(edges, lines, 1, Math.PI / 180, threshold, minLineLength, maxLineGap);
-        System.out.println("EDGES AKO: " + edges.cols());
-        System.out.println("I AM LINES COLS: " + lines.rows());
-        for(int i = 0; i < lines.rows(); i++) {
-            double[] val = lines.get(i, 0);
-            Imgproc.line(mRgba, new Point(val[0], val[1]), new Point(val[2], val[3]), new Scalar(255, 255, 0), 2);
-        }
+            //Convert back to MatOfPoint
+            MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
 
-        return mRgba; // This function must return
+            // Get bounding rect of contour
+            Rect rect = Imgproc.boundingRect(points);
+
+            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 0, 0, 255), 3);
+
+
+
+        }*/
+        return mRgba;
     }
+
 }
